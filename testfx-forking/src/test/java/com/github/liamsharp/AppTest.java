ebff0f27e124b3d794e152dc4e66cda102db6353
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2018-07-06 07:43:43 +0100

    Dont' call logApp method.

--- testfx-forking/src/test/java/com/github/liamsharp/AppTest.java
+++ testfx-forking/src/test/java/com/github/liamsharp/AppTest.java
@@ -1,8 +1,7 @@
 package com.github.liamsharp;
 
-import java.util.concurrent.TimeoutException;
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
@@ -14,19 +13,19 @@ public class AppTest
     private static final Logger LOGGER = LogManager.getLogger(AppTest.class);
 
     @Test
-    public void testStuff() throws TimeoutException, InterruptedException
+    public void testStuff()
     {
         assertEquals(1, App.returnOne());
         
         final Application app = null;
         LOGGER.info("app: " + app);
-        logApp(app);
+//        logApp(app);
     }
     
-    private void logApp(
-        final Application app)
-    {
-        LOGGER.info("app: " + app);
-    }
+//    private void logApp(
+//        final Application app)
+//    {
+//        LOGGER.info("app: " + app);
+//    }
 
 }
