c4d7317c67db238d42d8ec8d24fa335341c02b98
Liam Sharp <liam.sharp@screamingfrog.co.uk>
2018-07-06 07:32:21 +0100

    Remove deps on testfx so this is just a javafx demo app. Downgrade surefire to 2.12.4, no issue.

--- testfx-forking/src/main/java/com/github/liamsharp/App.java
+++ testfx-forking/src/main/java/com/github/liamsharp/App.java
@@ -1,72 +1,21 @@
 package com.github.liamsharp;
 
-import java.lang.management.ManagementFactory;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class App extends Application
+public class App 
 {
     private static final Logger LOGGER = LogManager.getLogger(App.class);
-    
-    private static int mCount = 0;
     
     public static void main(
         final String[] args) 
     {
-        launch(args);
-    }
-    
-    @Override
-    public void init() throws Exception
-    {
-        LOGGER.info("init being called: " + getPid());
-    }
-    
-    @Override
-    public void start(
-        final Stage primaryStage)
-    {
-        addShutdownHook();
-        mCount++;
-        LOGGER.info("App started! pid: " + getPid() + ", count: " + mCount);
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setId("clickme");
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(e -> LOGGER.info("Button clicked"));
-        
-        final StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        LOGGER.info("Hello world!");
     }
 
-    
-    public static String getPid()
+    public static int returnOne()
     {
-        String pid = "unknown";
-        final String jvmName = ManagementFactory.getRuntimeMXBean().getName();
-        final int index = jvmName.indexOf('@');
-        if (index != -1) 
-        {
-            pid = jvmName.substring(0, index);
-        }
-        return pid;
+        return 1;
     }
-    
-    private static void addShutdownHook() 
-    {
-        final Thread hookThread = new Thread(()-> LOGGER.info("Application Exited"));
-        hookThread.setName("exitlogger");
-        Runtime.getRuntime().addShutdownHook(hookThread);
-    }
-
 
 }
--- testfx-forking/src/test/java/com/github/liamsharp/AppTest.java
+++ testfx-forking/src/test/java/com/github/liamsharp/AppTest.java
@@ -2,28 +2,25 @@ package com.github.liamsharp;
 
 import java.util.concurrent.TimeoutException;
 
+import static org.junit.Assert.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.testfx.api.FxToolkit;
 
 import javafx.application.Application;
-import javafx.stage.Stage;
 
 public class AppTest
 {
     private static final Logger LOGGER = LogManager.getLogger(AppTest.class);
 
     @Test
-    public void testFxTest() throws TimeoutException, InterruptedException
+    public void testStuff() throws TimeoutException, InterruptedException
     {
-        LOGGER.info("AppTest started");
-        final Stage stage = FxToolkit.registerPrimaryStage();
-        final Application app = FxToolkit.setupApplication(App.class);
-        LOGGER.info("app: " + app + ", stage: " + stage);
+        assertEquals(1, App.returnOne());
+        
+        final Application app = null;
+        LOGGER.info("app: " + app);
         logApp(app);
-        FxToolkit.cleanupApplication(app);
-        LOGGER.info("App cleaned up");
     }
     
     private void logApp(
